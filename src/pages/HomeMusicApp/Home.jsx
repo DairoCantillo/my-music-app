51c802a1eda2ebd0930005eeeec406940c73f7b7
Dairo Cantillo <dairo.cantillo@pragma.com.co>
2022-02-07 17:34:23 -0500

    paleta de colores definida

--- src/pages/HomeMusicApp/Home.jsx
+++ src/pages/HomeMusicApp/Home.jsx
@@ -8,8 +8,9 @@ const Home = () => {
           "Bearer BQA7YB45MAVuVT3x2BQn3alsu8KELADLZVNnicNkMmjvaOxAAmEc_XEu5XT8H9o-MH6viu03H8MO4AgoyGZdm1EXC6Jpkkl76xi4Os0_Bn-v_1QpjIQvIXTlpLxhA2h-y9lqK6tkQ3msh-SFS7GFgfiQws8r1BP4dLX-q7GLVKWGVAMLzdM",
       };
       const params = {limit:50,offset:0};
-      const host = "https://api.spotify.com/v1/me/tracks";
-      const response = await axios.get(host, { headers,params });
+      const host = "https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF";
+    //   const host = "https://api.spotify.com/v1/me/tracks";tracks
+      const response = await axios.get(host, { headers });
       console.log("aver",response);
     } catch (error) {
       console.error(error);
