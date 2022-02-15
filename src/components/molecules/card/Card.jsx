01c71a2fa540a553eeea9be4eda01557417ad1cf
Dairo Cantillo <dairo.cantillo@pragma.com.co>
2022-02-15 08:39:03 -0500

    numero de caracteres por titulo limitado

--- src/components/molecules/card/Card.jsx
+++ src/components/molecules/card/Card.jsx
@@ -4,13 +4,21 @@ import DeleteButton from '../../atoms/delete-button/DeleteButton';
 
 
 const Card = ({ name, image, artist, id, type }) => {
+  const cutString = (string, limit)=>{
+    if(string.length>=limit){
+      const cuted = string.slice(0,limit);
+      return `${cuted}...`
+    }
+    return string;
+
+  }
   return (
     <>
       <div className="card-track" key={id}>
         <img className="card-track__image" src={image} alt={name} />
         <article className="card-track__info">
           <div>
-            <h3>{name}</h3>
+            <h3>{cutString(name, 24)}</h3>
             <p>{artist}</p>
           </div>
           {type === "like" ? <LikeButton id={id}/> : <DeleteButton id={id}/>}
