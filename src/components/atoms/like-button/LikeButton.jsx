b955192dfad7db147111b972cc622e705a087a89
Dairo Cantillo <dairo.cantillo@pragma.com.co>
2022-02-15 08:46:16 -0500

    animacion al dar like

--- src/components/atoms/like-button/LikeButton.jsx
+++ src/components/atoms/like-button/LikeButton.jsx
@@ -3,16 +3,33 @@ import dislike from "../../../common/assets/likeoff.png";
 import like from "../../../common/assets/likeon.png";
 import { useDispatch } from "react-redux";
 import FavoritesService from "../../../services/favoritesService";
+import { useState } from "react";
 const favoritesService = new FavoritesService();
 
 const LikeButton = ({ id }) => {
+  const [cliked, setCliked] = useState(false);
   const dispatch = useDispatch();
   const handleLike = (id) => {
+    setCliked(true);
     dispatch(favoritesService.addFavoriteTrack(id));
   };
   return (
     <>
-      <img className="like-button" onClick={() => handleLike(id)} src={dislike} alt="dislike" />
+      {cliked ? (
+        <img
+          className="like-button"
+          onClick={() => handleLike(id)}
+          src={like}
+          alt="dislike"
+        />
+      ) : (
+        <img
+          className="like-button"
+          onClick={() => handleLike(id)}
+          src={dislike}
+          alt="dislike"
+        />
+      )}
     </>
   );
 };
